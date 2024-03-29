cd13faedf3a584f5baef7a1c5f5f833b1e7c27d3
habtomQA <160462439+habtomQA@users.noreply.github.com>
2024-03-29 16:28:50 -0700

    worked homework17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,41 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
 
+import static org.bouncycastle.cms.RecipientId.password;
+
 public class BaseTest {
+    public WebDriver driver;
+    String url= "https://qa.koel.app/";
+
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    public void navigateToPage() {
+        driver.get(url);
+
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSubmit() {
+        WebElement submit = driver.findElement(By.cssSelector("input[type='password']"));
+        submit.click();
     }
 }
