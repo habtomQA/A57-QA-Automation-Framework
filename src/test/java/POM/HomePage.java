43143484188e12830b14453bf54fc604c9f5d19f
habtomQA <160462439+habtomQA@users.noreply.github.com>
2024-04-06 10:13:01 -0700

    These are for POM not for homework19

---
+++ src/test/java/POM/HomePage.java
@@ -0,0 +1,13 @@
+package POM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/*public class HomePage extends BasePage{
+    //Constructor
+    public HomePage(WebDriver giveDriver){
+        super(giveDriver)
+    }
+    //Locators
+    By userAvatarIcon=By.cssSelector("img.avatar")
+}*/
---
+++ src/test/java/POM/LoginPage.java
@@ -0,0 +1,20 @@
+package POM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/*public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver givenDriver){
+        super(givenDriver);
+
+    }
+    //Locator for Login Page Web elements
+    By emailField= By.cssSelector("input[type='email']");
+    By passwordField = By.cssSelector("input[type='password']");
+    By submitBtn = By.cssSelector("button[type='submit']");
+
+    //Methods
+
+
+}*/
