d2320f659bc9c24e98a8cf924aec1a0d514979e9
habtomQA <160462439+habtomQA@users.noreply.github.com>
2024-04-05 18:03:01 -0700

    homework19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+    @Test
+    public void deletePlaylist(){
+        //navigateToPage();
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickOnLoginBtn();
+        
+
+    }
+}
