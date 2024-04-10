3e41985514fd7253966700b6933725f59e8c4f68
habtomQA <160462439+habtomQA@users.noreply.github.com>
2024-04-10 00:34:03 -0700

    Threads removed from and URL declaration modified          on homework16

--- src/test/java/L16Tests.java
+++ src/test/java/L16Tests.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,30 +13,30 @@ public class L16Tests extends BaseTest{
 
 
     @Test(enabled = false,description = "Test skipped for a moment.")
-    public void loginEmptyEmailPassword() throws InterruptedException {
+    public void loginEmptyEmailPassword(String baseURL) {
         ChromeOptions options =new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver =new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        Thread.sleep(2000);
 
-        String url ="https://qa.koel.app/";
+
+        String url ="BaseURL";
         driver.get(url);
         // Assert.assertEquals(driver.getCurrentUrl(),url);
         //driver.quit();
-        Thread.sleep(2000);
-        WebElement emailField=driver.findElement(By.cssSelector("input[type='email']"));
+
+        WebElement emailField=wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
         emailField.clear();
         emailField.sendKeys("");
-        Thread.sleep(2000);
 
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
         passwordField.clear();
         passwordField.sendKeys("te$t$tudent");
-        Thread.sleep(2000);
 
-        WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        WebElement loginBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
         loginBtn.click();
 
         Assert.assertEquals(driver.getCurrentUrl(),url);
@@ -50,21 +51,21 @@ public class L16Tests extends BaseTest{
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        Thread.sleep(2000);
-        String url="https://qa.koel.app/";
+
+        String url="BaseURL";
         driver.get(url);
-        Thread.sleep(2000);
+
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
         emailField.sendKeys("invalidemail@class.com");
-        Thread.sleep(2000);
+
         WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
         passwordField.clear();
         passwordField.sendKeys("te$tStudent");
-        Thread.sleep(2000);
+
         WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
         loginBtn.click();
-        Thread.sleep(2000);
+
         Assert.assertEquals(driver.getCurrentUrl(),url);
 
         driver.quit();
@@ -78,7 +79,7 @@ public class L16Tests extends BaseTest{
         providePassword("te$t$tudent");
         clickLoginBtn();
 
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        WebElement avatarIcon =wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
         Assert.assertTrue(avatarIcon.isDisplayed());
     }
 
@@ -86,8 +87,9 @@ public class L16Tests extends BaseTest{
 
 
     @Test
-    public void loginInvalidEmailPassword(){
+    public void loginInvalidEmailPassword(String baseURL){
         //navigateToPage(String url);
+        String url="BaseURL";
         provideEmail("invalidemail@class.com");
         providePassword("te$tStudent");
         clickLoginBtn();
